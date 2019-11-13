16d146242773f78838aebe75a7005643d5177edc
renatobarbosa1997 <56805018+renatobarbosa1997@users.noreply.github.com>
2019-11-13 16:13:03 -0300

    Cronometro working

--- resources/views/pedidostela/lista.blade.php
+++ resources/views/pedidostela/lista.blade.php
@@ -2,8 +2,6 @@
 
 @section('conteudo')
 
-<div style="float: left;">
-<br><h2> Pedidos pendentes</h2>
 
 <style>
 table {
@@ -42,7 +40,21 @@ tr:nth-child(even) {
   cursor: pointer;
   margin-top: 50px;
 }
+
+#screen {
+    font-family: Calibri,Arial;
+    font-weight: 300;
+    font-size: 45px;
+    width: 330px;
+    height: 50px;
+    color: gray;
+    letter-spacing: 3px;
+}
 </style>
+
+
+<div style="float: left; margin-left: 100px">
+<br><h2> Pedidos pendentes</h2>
 
 
 <table width="250px";>
@@ -76,9 +88,9 @@ tr:nth-child(even) {
 </div>
 
 <div style="float: right; margin-right: 130px;">
-    <button class="button">Finalizar</button>
-    <button class="button">Iniciar</button>
-    <button class="button">Cancelar</button>
+    <button class="button" onclick="reset()">Finalizar &#9745;</button>
+    <button class="button" onclick="start()">Iniciar &#9658;</button>
+    <button class="button" onclick="stop()">Suspender &#8718;</button>
 </div>
 
 <div style="float: right; margin-right: -320px;  margin-top: 190px">
@@ -105,8 +117,69 @@ tr:nth-child(even) {
 </table>
 </div>
 
-<div style="float: left; margin-top: 250px">
-<h1>CRONOMETRO</h1>
+<div style="float: left; margin-top: 150px">
+<div class="chronometer">
+        <div id="screen" style="margin-left: 100px;">00 : 00 : 00 : 00</div>
+    </div>
 </div>
+
+<script type="text/javascript">
+    window.onload = function() {
+   pantalla = document.getElementById("screen");
+}
+var isMarch = false; 
+var acumularTime = 0; 
+function start () {
+         if (isMarch == false) { 
+            timeInicial = new Date();
+            control = setInterval(cronometro,10);
+            isMarch = true;
+            }
+         }
+function cronometro () { 
+         timeActual = new Date();
+         acumularTime = timeActual - timeInicial;
+         acumularTime2 = new Date();
+         acumularTime2.setTime(acumularTime); 
+         cc = Math.round(acumularTime2.getMilliseconds()/10);
+         ss = acumularTime2.getSeconds();
+         mm = acumularTime2.getMinutes();
+         hh = acumularTime2.getHours()-21;
+         if (cc < 10) {cc = "0"+cc;}
+         if (ss < 10) {ss = "0"+ss;} 
+         if (mm < 10) {mm = "0"+mm;}
+         if (hh < 10) {hh = "0"+hh;}
+         pantalla.innerHTML = hh+" : "+mm+" : "+ss+" : "+cc;
+         }
+
+function stop () { 
+         if (isMarch == true) {
+            clearInterval(control);
+            isMarch = false;
+            }     
+         }      
+
+function resume () {
+         if (isMarch == false) {
+            timeActu2 = new Date();
+            timeActu2 = timeActu2.getTime();
+            acumularResume = timeActu2-acumularTime;
+            
+            timeInicial.setTime(acumularResume);
+            control = setInterval(cronometro,10);
+            isMarch = true;
+            }     
+         }
+
+function reset () {
+         if (isMarch == true) {
+            clearInterval(control);
+            isMarch = false;
+            }
+         acumularTime = 0;
+         pantalla.innerHTML = "00 : 00 : 00 : 00";
+         }
+
+</script>
 
 @stop
